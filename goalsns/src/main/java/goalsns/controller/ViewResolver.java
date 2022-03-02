208573d5ee4f1ef0f2472406f46c4a33f0486617
smhrd <smhrd@211.227.114.104>
2022-03-02 11:56:37 +0900

    viewresolver 수정

--- goalsns/src/main/java/goalsns/controller/ViewResolver.java
+++ goalsns/src/main/java/goalsns/controller/ViewResolver.java
@@ -2,6 +2,6 @@ package goalsns.controller;
 
 public class ViewResolver {
 	public static String makeURL(String nextView) {
-		return "WEB-INF/member/"+nextView+".jsp";
+		return nextView+".jsp";
 	}
 }
