320a1c13a9804e46937fabd346b916760bbc174d
smhrd <smhrd@121.147.185.227>
2022-03-03 14:05:00 +0900

    수정

--- goalsns/src/main/java/goalsns/controller/HandlerMapping.java
+++ goalsns/src/main/java/goalsns/controller/HandlerMapping.java
@@ -10,6 +10,7 @@ public class HandlerMapping {
 		//¿¹½Ã - mappings.put("/boardList.do", new BoardListController());
 		mappings.put("/postWrite.po", new PostWriteController());
 		mappings.put("/login.do", new MemberLoginController());
+		mappings.put("/main.do", new MainController());
 		mappings.put("/postList.po", new PostListController());
 	}
 	public Controller getController(String key) {
--- goalsns/src/main/java/goalsns/model/MemberDAO.java
+++ goalsns/src/main/java/goalsns/model/MemberDAO.java
@@ -114,7 +114,6 @@ public class MemberDAO {
 	}
 	public List<MemberVO> selectAll() {
 		SqlSession session=sqlSessionFactory.openSession();
-		
 		List<MemberVO> list=session.selectList("selectAll");
 		session.close(); //¹Ý³³
 		return list;
