593e79c3193ef36901c16d58411d7ec9fceec0ad
smhrd <smhrd@211.227.114.104>
2022-03-03 09:43:29 +0900

    post

--- goalsns/src/main/java/goalsns/model/PostDAO.java
+++ goalsns/src/main/java/goalsns/model/PostDAO.java
@@ -1,5 +1,30 @@
 package goalsns.model;
 
-public class PostDAO {
+import java.io.InputStream;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import goalsns.entity.PostVO;
+
+public class PostDAO {
+	private static SqlSessionFactory sqlSessionFactory;
+	//초기화 블럭.(프로그램 실행시 한번만 실행되는 부분.)
+	static {
+		try {
+			String resource = "goalsns/model/config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void postwrite(PostVO vo) {
+		SqlSession session = sqlSessionFactory.openSession();
+		session.insert("postwrite", vo);
+		session.commit();
+		session.close();
+	}
 }
