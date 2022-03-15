5eb68be255f930fb5aa0d4dc4b172a1cb09e77eb
smhrd <smhrd@121.147.185.227>
2022-03-15 09:26:59 +0900

    profiledefault 수정 idCheck ajax추가

--- goalsns/src/main/java/goalsns/controller/ProfileDefaultController.java
+++ goalsns/src/main/java/goalsns/controller/ProfileDefaultController.java
@@ -24,27 +24,19 @@ public class ProfileDefaultController implements Controller {
 		MemberVO memvo = (MemberVO)session.getAttribute("memvo");
 		String id = (String)request.getParameter("mem_id");//상대방아이디
 		String from_mem=(String)memvo.getMem_id();//자신의아이디
-		System.out.println(id);
-		System.out.println(from_mem);
 		MemberDAO mdao = new MemberDAO();
 		PostDAO pdao = new PostDAO();
 		FollowVO fvo=new FollowVO();
 		fvo.setTo_mem(id);
 		fvo.setFrom_mem(from_mem);
 		MemberVO mvo = mdao.getMemberInfo(id);
-		List<FollowVO> memfo=mdao.getFollowInfo(fvo);
-		//List<FollowVO> followlist=mdao.followAll(fvo);
 		List<FollowVO> tofollowlist=mdao.tofollowAll(fvo);
-		//List<FollowVO> followedlist=mdao.followedAll(fvo);
 		List<FollowVO> tofollowedlist=mdao.tofollowedAll(fvo);
 		List<PostVO> postList = pdao.getMemberPosts(id);
 		int postCnt = postList.size();
 		request.setAttribute("postList", postList);
 		request.setAttribute("postCnt", postCnt);
 		request.setAttribute("mvo", mvo);
-		request.setAttribute("memfo", memfo);
-		//request.setAttribute("followlist", followlist);
-		//request.setAttribute("followedlist", followedlist);
 		request.setAttribute("tofollowlist", tofollowlist);
 		request.setAttribute("tofollowedlist", tofollowedlist);
 		return "profile";
